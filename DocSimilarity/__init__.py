be3d91605d66501eaa6948e97e1eff479926c09a
Malintha <malintha@wso2.com>
2016-11-27 00:38:19 +0530

    Initial commit

--- DocSimilarity/__init__.py
+++ DocSimilarity/__init__.py
@@ -1,2 +1,9 @@
-import nltk
-help(nltk)
+from nltk.corpus import stopwords
+
+# stop word sample
+stop = stopwords.words('english')
+
+sentence = 'this is a foo bas sentence'
+
+print [i for i in sentence.split() if i not in stop]
+
